e1aec689b9a08599d3df75d322a69eff66157a42
bit-w1zard <adelkhalid.tx@gmail.com>
2022-08-18 03:51:36 -0500

    Created databse models

---
+++ api/models/poi.py
@@ -0,0 +1,19 @@
+from simple_history.models import HistoricalRecords
+from django.db import models
+from .utils import TimestampMixin
+
+class PoI(TimestampMixin):
+    poi_id = models.CharField(max_length=100, verbose_name='poi external id')
+    poi_name = models.CharField(max_length=255)
+    poi_category = models.CharField(max_length=100)
+    poi_latitude = models.FloatField()
+    poi_longitude = models.FloatField()
+    poi_ratings = models.JSONField()
+    
+    def __str__(self):
+        return self.poi_name
+    class Meta:
+        verbose_name = "POI"
+        verbose_name_plural = "POIs"
+    
+    history = HistoricalRecords()
